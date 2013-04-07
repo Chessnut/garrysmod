50b9796616e5f6843cff990df70bc9f33241d25a
Chessnut <chessnutist@gmail.com>
2013-04-07 08:50:40 -0700

    Update arming.lua
    Calls CanArmDupe hook.

--- garrysmod/gamemodes/sandbox/entities/weapons/gmod_tool/stools/duplicator/arming.lua
+++ garrysmod/gamemodes/sandbox/entities/weapons/gmod_tool/stools/duplicator/arming.lua
@@ -1,4 +1,3 @@
-
 if ( CLIENT ) then
 
 	--
@@ -48,8 +47,12 @@ if ( SERVER ) then
 
 			if ( !IsValid( client ) ) then return end
 
-			-- Hook.. can arn dupe..
-
+			-- Hook.. can arm dupe..
+			-- Pass true so it is known the dupe is from Workshop.
+			if (hook.Run("CanArmDupe", client, true) == false) then
+				return;
+			end;
+			
 			local uncompressed = util.Decompress( data )
 			if ( !uncompressed ) then 
 				MsgN( "Couldn't decompress dupe!" )
